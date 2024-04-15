96d12c98583e5548a59a015459003e295644d12b
Omkar Potphode <omkar03potphode@gmail.com>
2024-04-15 16:06:16 +0530

    🍱 Added logo and images

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import Logo from './images/Logo.svg';
+import BlueWave from './images/Graphic-Blue-Wave.svg';
+import PurpleWave from './images/Graphic-Purple-Wave.svg';
+import TealWave from './images/Graphic-Teal-Wave.svg';
+import HeroImg from './images/Graphic-Hero-Blur.jpg';
+
+export {Logo, BlueWave, PurpleWave, TealWave, HeroImg};
